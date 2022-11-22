c3a7d8917f87cf1383a37af266a1b60b3cff8f68
Thiago Costa <37018497+thiscosta@users.noreply.github.com>
2022-11-22 19:17:30 -0300

    Imported from Quest

--- src/components/ImageUploadTest/useImageUploadTest.ts
+++ src/components/ImageUploadTest/useImageUploadTest.ts
@@ -17,6 +17,8 @@ import React from 'react'
 const variantOptions = { Property1Default: "Property1Default" , Property1Variant2: "Property1Variant2" , Property1Variant3: "Property1Variant3"  }
 
 const useImageUploadTest = () => {
+  const test = (): any => {};
+  const onVisible = (param1: any): any => {};
   const [currentVariant, setCurrentVariant] = React.useState<string>(variantOptions['Property1Default']);
       const [previousVariant, setPreviousVariant] = React.useState<string>('');
   let data: any = { currentVariant, previousVariant };
